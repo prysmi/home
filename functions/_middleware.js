1dfa51345218825b81102f04e3ccb6da582cb2a6
prysmi <prabhjot@prysmi.com>
2025-08-29 02:26:23 +0530

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,5 @@
 // functions/_middleware.js
-/*
+
 export async function onRequest(context) {
   // Fetch the original page
   const response = await context.env.ASSETS.fetch(context.request);
@@ -46,5 +46,6 @@ export async function onRequest(context) {
   return newResponse;
 }
 
-*/
+
+
 
